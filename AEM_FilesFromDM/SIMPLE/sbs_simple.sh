e0e6c776ce009d818132cd772b2e337ddcb099c0
Pete <petess@gmail.com>
2017-06-19 11:26:52 +1000

    Added extra template files

---
+++ AEM_FilesFromDM/SIMPLE/sbs_simple.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#PBS -P y33
+#PBS -q normal
+#PBS -l ncpus=8
+#PBS -l mem=8GB
+#PBS -l walltime=2:00:00
+#PBS -l wd
+#PBS -N galeisbstdem
+#PBS -o galeisbstdem.out
+#PBS -e galeisbstdem.err
+#PBS -j oe
+
+module load ga-aem/1.0
+mpirun galeisbstdem.exe SIMPLE.con
+
